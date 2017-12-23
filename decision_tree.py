cd25096c4030c6066135715c316cb07784b10027
Prophet <reganyang0415@gmail.com>
2017-12-23 18:46:23 +1100

    add python file

---
+++ decision_tree.py
@@ -0,0 +1,6 @@
+#!usr/bin/python3
+# this is a practice for decision tree in machine learning.
+# written by Prophet Young at 18:44 23/12/2017.
+# test environment:
+# Python 3.6.3 (v3.6.3:2c5fed86e0, Oct  3 2017, 00:32:08) 
+# [GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
